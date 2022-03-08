df69871ab861d41ec96ac50200490a7251be4636
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-03-08 12:28:54 +0300

    added config

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
-from flask_migrate import Migrate, MigrateCommand
+from flask_mrate  import Migrate, MigrateCommand
 #Creating app instance
 app = create_app('production')
 manager = Manager(app)
