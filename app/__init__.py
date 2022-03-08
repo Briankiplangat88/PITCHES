a96538d5fda277f34b7c34be71471a193f27104a
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-03-08 10:46:04 +0300

    updated views

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_ogin import LoginManager
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ export MAIL_USERNAME='bryanbryson85@gmail.com'
 export MAIL_PASSWORD='124512bK'
 export SECRET_KEY='secret'
 export SQLALCHEMY_DATABASE_URI= ""
-# SQLALCHEMY_DATABASE_URI='DATABASE_URL'
-python3.9 manage.py server
+  # SQLALCHEMY_DATABASE_URI='DATABASE_URL'
+  python3.9 manage.py server
